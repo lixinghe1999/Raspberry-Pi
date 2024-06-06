734fcc433c4d2abef16a77be770d319420321844
lixinghe1999 <helixing99@gmail.com>
2024-06-06 16:18:35 +0800

    1

--- deaf_space/record.py
+++ deaf_space/record.py
@@ -16,22 +16,24 @@ def record(output_file, recording_duration=5):
 if __name__ == '__main__':
     import argparse
     import datetime
-    import multiprocessing
+    from multiprocessing import Process
     parser = argparse.ArgumentParser(description='Record audio and video')
     parser.add_argument('--sensors', '-s', default=['earphone', 'micarray'], nargs='+', required=False)
     parser.add_argument('--duration', '-d', default=5, type=int, required=False)
 
     args = parser.parse_args()
     output_file = datetime.datetime.now().strftime('%Y%m%d_%H%M%S')
-    func_list = []
+    process_list = []
     for sensor in args.sensors:
         if sensor == 'earphone':
-            func_list.append([record_audio, (output_file + '_earphone', 1, 2, args.duration)])
+            p = Process(target=record_audio, args=(output_file + '_earphone', 1, 2, args.duration))
+            process_list.append(p)
         elif sensor == 'micarray':
-            func_list.append([record_audio, (output_file + '_micarray', 2, 8, args.duration)])
-        elif sensor == 'imu':
-            func_list.append(record_imu)
+            p = Process(target=record_audio, args=(output_file + '_micarray', 2, 8, args.duration))
+            process_list.append(p)
         else:
-            func_list.append(record)
-    
-    multiprocessing.Pool(len(func_list)).map(lambda x: x[0](*x[1]), func_list)
+            pass
+    for p in process_list:
+        p.start()
+    for p in process_list:
+        p.join()
