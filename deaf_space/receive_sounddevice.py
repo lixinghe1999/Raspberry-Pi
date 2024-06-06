2526deae19e6526aa682c1720de28bff69b7e8c0
lixinghe1999 <helixing99@gmail.com>
2024-06-06 16:20:50 +0800

    1

--- deaf_space/receive_sounddevice.py
+++ deaf_space/receive_sounddevice.py
@@ -5,33 +5,11 @@ import numpy as np
 def record(output_file, device=1, channels=2, recording_duration=5):
     # Audio settings
     audio_rate = 44100
-    audio_frames_per_buffer = 1024
     audio_output = output_file + '.flac'
 
-    # Initialize audio recording
-    audio_frames = []
-    total_audio_frames = int(audio_rate * recording_duration) // audio_frames_per_buffer
+    audio_data = sd.rec(int(audio_rate * recording_duration), samplerate=audio_rate, channels=channels, device=device, blocking=True)
 
-    def audio_callback(indata, frames, time, status):
-        audio_frames.append(indata.copy())
-
-    # Start audio recording
-    audio_stream = sd.InputStream(channels=channels,
-                                samplerate=audio_rate,
-                                callback=audio_callback, device=device)
-
-    audio_stream.start()
-
-    # Recording loop
-    while len(audio_frames) < total_audio_frames:
-        pass
-
-    # Stop audio recording
-    audio_stream.stop()
-
-    # Save audio recording
-    audio_frames = np.concatenate(audio_frames, axis=0)
-    sf.write(audio_output, audio_frames, audio_rate)
+    sf.write(audio_output, audio_data, audio_rate)
     print('Audio recording saved to', audio_output)
 
 if __name__ == '__main__':
