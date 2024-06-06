f4be8ea941f5afeb7a241b54e1c3070db4e7911f
lixinghe1999 <helixing99@gmail.com>
2024-06-06 18:32:21 +0800

    1

--- deaf_space/record.py
+++ deaf_space/record.py
@@ -26,8 +26,8 @@ if __name__ == '__main__':
 
     command_array = 'arecord -Dac108 -f S32_LE -r 16000 -c 8 -d {} {}.wav'.format(args.duration, output_file + '_micarray').split()
     command_earphone = 'arecord -Dhw:1,0 -f S16_LE -r 16000 -c 2 -d {} {}.wav'.format(args.duration, output_file + '_earphone').split()
-    subprocess.run(command_array)
-    subprocess.run(command_earphone)
+    subprocess.Popen(command_array)
+    subprocess.Popen(command_earphone)
 
 #     from multiprocessing import Process
 #    
